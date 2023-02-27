722e2876929e8e137a61433b2a36407a985ed5c8
0x3E5 <4722957@qq.com>
2023-02-27 15:10:26 +0800

    升级依赖后调整配置

--- client/vue.config.js
+++ client/vue.config.js
@@ -1,3 +1,11 @@
+/*
+ * @Author: 0x3E5
+ * @Date: 2023-02-27 14:56:02
+ * @LastEditTime: 2023-02-27 15:08:02
+ * @LastEditors: 0x3E5
+ * @Description: 
+ * @FilePath: \orderSYS\client\vue.config.js
+ */
 const debug = process.env.NODE_ENV !== 'production'
 
 module.exports = {
@@ -8,28 +16,6 @@ module.exports = {
   runtimeCompiler: true, // 运行时版本是否需要编译
   transpileDependencies: [], // 默认babel-loader忽略mode_modules，这里可增加例外的依赖包名
   productionSourceMap: true, // 是否在构建生产包时生成 sourceMap 文件，false将提高构建速度
-  configureWebpack: config => { // webpack配置，值位对象时会合并配置，为方法时会改写配置
-    if (debug) { // 开发环境配置
-      config.devtool = 'cheap-module-eval-source-map'
-    } else { // 生产环境配置
-    }
-    // Object.assign(config, { // 开发生产共同配置
-    //     resolve: {
-    //         alias: {
-    //             '@': path.resolve(__dirname, './src'),
-    //             '@c': path.resolve(__dirname, './src/components'),
-    //             'vue$': 'vue/dist/vue.esm.js'
-    //         }
-    //     }
-    // })
-  },
-  chainWebpack: config => { // webpack链接API，用于生成和修改webapck配置，https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
-    if (debug) {
-      // 本地开发配置
-    } else {
-      // 生产开发配置
-    }
-  },
   parallel: require('os').cpus().length > 1, // 构建时开启多进程处理babel编译
   pluginOptions: { // 第三方插件配置
   },
@@ -40,7 +26,6 @@ module.exports = {
     host: 'localhost',
     port: 8080,
     https: false,
-    hotOnly: false,
     proxy: { // 配置跨域
       '/api': {
         target: 'http://localhost:3000/api/',
@@ -50,8 +35,7 @@ module.exports = {
           '^/api': ''
         }
       }
-    },
-    before: app => { }
+    }
   },
   transpileDependencies: [
     'vue-echarts',
